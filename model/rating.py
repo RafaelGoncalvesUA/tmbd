26c27be120fb83ca0785ab81e8649453d75bcb44
Rafael Fernandes Gonçalves <rfg@ua.pt>
2025-01-15 19:40:48 +0000

    Add experiments

--- model/rating.py
+++ model/rating.py
@@ -6,9 +6,15 @@ all_categories = ['Action', 'Adventure', 'Animation', "Family", 'Comedy', 'Crime
               'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Music', 'Mystery', 'Romance',
               'Science Fiction', 'Thriller', 'War', 'Western']
 
-def predict_rating_for_user(rating_regressor, categories, user):
+def predict_rating_for_user(rating_regressor, categories, user_preferences_mean):
     cat_features = {cat: 1 if cat in categories else 0 for cat in all_categories}
-    user = {col + '_user_pref' if col in all_categories else col: val for col, val in user.items()} 
+    
+    user = {
+        col + '_user_pref'
+        if col in all_categories else col: val
+        for col, val in user_preferences_mean.items()
+    } 
+    
     input_data = {**cat_features, **user}
     input_df = pd.DataFrame([input_data], index=[0])
     return rating_regressor.predict(input_df)[0]
